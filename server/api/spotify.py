025bba1be6f6147391ea10133ff3c3dc40cd9b60
Antonio Augusto Brito de Sousa <up202000705@fe.up.pt>
2024-07-11 00:38:23 +0100

    Spotify API integrated and API search implemented

---
+++ server/api/spotify.py
@@ -0,0 +1,72 @@
+from credentials import CLIENT_SECRET, CLIENT_ID
+import requests
+import base64
+import datetime
+from urllib.parse import urlencode
+from utils.json_handler import info_retriever
+class SpotifyAPI:
+    lastToken = None
+    def __init__(self):
+        if SpotifyAPI.lastToken is None or self.token_expired():
+            self.refresh_token()
+        
+    def token_expired(self):
+        if SpotifyAPI.lastToken is not None:
+            now = datetime.datetime.now()
+            expiration_time = SpotifyAPI.lastToken['expires_in']
+            token_time = SpotifyAPI.lastToken['timestamp']
+            if now - token_time >= datetime.timedelta(seconds=expiration_time):
+                return True
+        return False
+
+    def refresh_token(self):
+        client_creds = f"{CLIENT_ID}:{CLIENT_SECRET}"
+        client_creds_b64 = base64.b64encode(client_creds.encode())
+        token_url = "https://accounts.spotify.com/api/token"
+        method = "POST"
+        token_data = {
+            "grant_type": "client_credentials"
+        }
+
+        token_headers = {
+            "Authorization": f"Basic {client_creds_b64.decode()}"
+        }
+        req = requests.post(token_url, data=token_data, headers=token_headers)
+        token_response_data = req.json()
+        access_token = token_response_data['access_token']
+        expires_in = token_response_data['expires_in']
+        token_type = token_response_data['token_type']
+        self.lastToken = {
+            'access_token': access_token,
+            'expires_in': expires_in,
+            'token_type': token_type,
+            'timestamp': datetime.datetime.now()
+        }
+        
+    
+    def search(self,query):
+        if self.token_expired():
+            self.refresh_token()
+        headers = {
+            "Authorization": f"Bearer {self.lastToken['access_token']}"
+        }
+        endpoint = "https://api.spotify.com/v1/search"
+        data = urlencode({"q": query, "type": "artist,album", "limit": 2, 'market': 'US'})
+        
+        lookup_url = f"{endpoint}?{data}"
+        
+        req = requests.get(lookup_url, headers=headers)
+        
+        response = info_retriever(req.json())
+        return response
+        
+   
+# if __name__ == "__main__":
+#     print('rodando...')
+#     spotify = SpotifyAPI()
+#     spotify.search("Offspring")
+    
+
+
+
+
---
+++ server/api/utils/json_handler.py
@@ -0,0 +1,29 @@
+import json
+
+def info_retriever(jsonData):
+    extracted = []
+    
+    for item in jsonData:
+        if item == 'artists':
+            for artist in jsonData[item]['items']:
+                extracted.append({
+                    'name': artist['name'],
+                    'id': artist['id'],
+                    'type': 'artist',
+                    'id': artist['id'],
+                    'images': artist['images'][0]['url'] if artist['images'] else None,
+                    'uri': artist['uri']
+                })
+        if item == 'albums':
+            for album in jsonData[item]['items']:
+                extracted.append({
+                    'name': album['name'],
+                    'id': album['id'],
+                    'type': 'album',
+                    'artist': album['artists'][0]['name'],
+                    'artist_id': album['artists'][0]['id'],
+                    'images': album['images'][1] if album['images'] else None,
+                    'release_date': album['release_date'],
+                    'uri': album['uri']            
+                })
+    return extracted
