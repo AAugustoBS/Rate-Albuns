6b829224813c9ba573b54f3ae9d99e0d6c669de4
Antonio Augusto Brito de Sousa <up202000705@fe.up.pt>
2024-07-11 18:21:04 +0100

    .git ignore modified

--- server/api/credential.py
+++
@@ -1,5 +0,0 @@
-# credential.py
-CLIENT_SECRET = '0df6dec2d2974654a9b913becacfe0c4'
-CLIENT_ID = 'ef1f5c63b8834d9ca4d250ffeb9eca82'
-if __name__ == "__main__":
-    print("This is a credential file")
